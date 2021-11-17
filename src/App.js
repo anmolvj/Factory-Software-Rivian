d2081c47263f48c3a4fbebd617f1be752148d9ec
anmol.vijayvargiya <anmol.vijayvargiya@servicenow.com>
2021-11-16 22:54:27 -0800

    Revert "structure code"
    This reverts commit 17e6f69336edb4049d9eb035f1427fd336600497.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { useReducer } from "react";
-import { ProductionLine } from "./components/styled";
-import { initialState, reducer } from "./store/index";
-import Station from "./components/Station";
-import { STATUSES } from "./constants/statuses";
-
+import { ProductionLine } from "./StyledComponents";
+import { initialState, reducer } from "./state";
+import Station from "./Station";
+import { STATUSES } from "./constants";
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const handleAddVehicle = (key) => () =>
     key === "1" ? dispatch({ type: "ADD_VEHICLE" }) : null;
-
+    
   const handleMoveVehicle = (key) => () =>
     dispatch({ type: "MOVE_VEHICLE", current_station: parseInt(key) });
 
   const handleVehicleStatusChange = (key) => (newStatus) => {
-    dispatch({
-      type: "UPDATE_VEHICLE_STATUS",
-      current_station: parseInt(key),
-      newStatus,
-    });
-  };
+    dispatch({type: "UPDATE_VEHICLE_STATUS", current_station: parseInt(key), newStatus})
+  }
   return (
     <div className="App">
       <ProductionLine>
